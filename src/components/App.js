c6f7065e65857d0c261a20548ca5cac37dfe8d5b
lri <lri@LRI-TYPE-R>
2017-05-24 17:05:49 +0900

    back to top add

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,30 @@ import {
   ResponsiveAppBar
 } from 'material-ui-responsive-drawer'
 import FlatButton from 'material-ui/FlatButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ArrowUpward from 'material-ui/svg-icons/navigation/arrow-upward';
+import Back2Top from 'react-back2top';
 
 const styles = {
   body_header: {
     margin: '0px',
     padding: '10px'
+  },
+
+  floating_arrow: {
+    margin: 0,
+    top: 'auto',
+    right: 20,
+    bottom: 20,
+    left: 'auto',
+    position: 'fixed',
+    zIndex: '9999'
   }
+
 }
 
 const App = () => (
@@ -20,12 +38,27 @@ const App = () => (
     <BodyContainer>
       <ResponsiveAppBar
         title={'Matome'}
-        iconElementRight={<FlatButton label="Demo" />}
+        iconElementRight={
+          <IconMenu
+            iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+            anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+            targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+          >
+            <MenuItem primaryText="Refresh" />
+            <MenuItem primaryText="Send feedback" />
+            <MenuItem primaryText="Settings" />
+            <MenuItem primaryText="Help" />
+            <MenuItem primaryText="Sign out" />
+          </IconMenu>
+        }
       />
       <div style={{ margin: '10px' }}>
         <h1 style={styles.body_header}>Body</h1>
         <ItemList />
       </div>
+      <Back2Top>
+        <FloatingActionButton style={styles.floating_arrow}><ArrowUpward /></FloatingActionButton>
+      </Back2Top>
     </BodyContainer>
   </div>
 )
